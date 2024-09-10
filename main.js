22294b251cc125d2fac3f1782f87d25db464f84b
teuzada <163007822+teuzada@users.noreply.github.com>
2024-09-10 08:38:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const startButton = document.querySelector(".start");
+const gameOverScreen = document.querySelector(".gameover");
+
+let GameStarted = false;
+
+const startGame = () => {
+gameStarted = true;
+
+pipe.style.animation = "pipe-animation 1.5s infinite"
+startButton.style.display = "none';
+    mario.style.opacity = "1";
+    pipe.style.opacity = "1";
+    clouds.style.opacity = "1";
+}
 
 const jump = () => {
     mario.classList.add("jump");
