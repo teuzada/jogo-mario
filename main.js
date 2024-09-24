b06287076fdb9a3fcbdfe9d71a20b8eaab101098
teuzada <163007822+teuzada@users.noreply.github.com>
2024-09-24 08:21:28 -0300

    .

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 const scoreElement = document.querySelector(".score");
 
-audioStart = new Audio("./sound/audio_theme.mp3");
-const gameOverSound = new Audio("./sound/audio_gameover.mp3");
+audioStart = new Audio("./sound/pou.mp3");
+const gameOverSound = new Audio("./sound/oof.mp3");
 
 let gameStarted = false;
 let score = 0;
